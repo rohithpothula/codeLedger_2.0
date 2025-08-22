6326bc5aeabf95a75daff5090c5089328925998b
Rohith Kumar Reddy Pothula <pothularohithkumarreddy@gmail.com>
2025-08-22 17:46:46 +0530

    Testing

--- SingletonDesingPattern/src/main/java/org/example/Singleton.java
+++ SingletonDesingPattern/src/main/java/org/example/Singleton.java
@@ -11,6 +11,7 @@ public class Singleton {
             synchronized (Singleton.class) {
                 if(singletonInstance==null){
                     singletonInstance= new Singleton();
+
                 }
             }
         }
