b66e05f2055072a6702e74c283a4b07563aa4373
Rohith Kumar Reddy Pothula <pothularohithkumarreddy@gmail.com>
2025-08-28 23:50:15 +0530

    Bill Pugh Singleton Pattern

---
+++ SingletonDesingPattern/src/main/java/org/example/Bill_Pugh_Singleton_pattern.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Bill_Pugh_Singleton_pattern {
+
+    private Bill_Pugh_Singleton_pattern{};
+
+    public static class Bill_Pugh_Singleton_pattern_helper{
+        private static final Bill_Pugh_Singleton_pattern billPughSingletonPattern_Instance = new Bill_Pugh_Singleton_pattern();
+    }
+
+    public static Bill_Pugh_Singleton_pattern getInstance(){
+        return Bill_Pugh_Singleton_pattern_helper.billPughSingletonPattern_Instance;
+    }
+}
